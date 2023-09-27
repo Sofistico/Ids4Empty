d3264706424f745b793604bde2915dd3d5b54ad7
João Victor Santos Rodrigues <joaorodrigues@bne.com.br>
2023-09-27 17:12:46 -0300

    Adicionar arquivos de projeto.

---
+++ Ids4AdmUI/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Ids4AdmUI.Models
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
